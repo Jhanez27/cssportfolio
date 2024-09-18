db4c5e2283708deddf38d373f0657cc588141d09
Rigen Suringa <rigensuriunga2@gmail.com>
2024-09-19 01:52:01 +0800

    home

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        teal: "#115E59",
+        "teal-100": "#173B45",
+      },
+    },
+  },
+  plugins: [],
+};
